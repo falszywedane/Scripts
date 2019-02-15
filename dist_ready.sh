4fe60f5adeb835e5f8db5a3a64d7465312aa7d58
root <root@localhost.localdomain>
2019-02-15 04:04:53 -0500

    Background setting fix

--- dist_ready.sh
+++ dist_ready.sh
@@ -389,7 +389,7 @@ echo -e "\n      \e[34m--- STARTTUP SCRIPT CHMOD CHANGE ---\e[39m"
 
 echo -e "\n      \e[34m--- SETTING WALLPAPER UP ---\e[39m"
 {
-	wget https://i2.wp.com/wodkiewicz-sosnowski.pl/wp-content/uploads/2018/09/pedro-lastra-157071-unsplash.jpg?ssl=1 -O /root/Pictures/tapeta.jpeg
+	wget https://i2.wp.com/wodkiewicz-sosnowski.pl/wp-content/uploads/2018/09/pedro-lastra-157071-unsplash.jpg?ssl=1 -O /root/Pictures/tapeta.jpg
 	gsettings set org.gnome.desktop.background picture-uri file:///root/Pictures/tapeta.jpg
  
 } || {
